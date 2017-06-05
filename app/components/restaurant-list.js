7910f608a95477ba6d544eae5b5e4ab6261f2504
vanduong28 <vanduong28@gmail.com>
2017-06-05 13:21:48 -0400

    Add form to create/add restaurant to list
    Add form to create restaurant entry in db. Need to clear form after
    submission and only display one form instead of one for each entry.
    City attribute appears to be passed correctly for restaurants created
    using form. Not sure why it is not passed for restaurants created using
    curl scripts.

--- app/components/restaurant-list.js
+++ app/components/restaurant-list.js
@@ -1,10 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  newRestaurant: {
+    name: null,
+    city: null,
+    cuisine: null,
+    neighborhood: null,
+    date_dined: null,
+    notes: null
+  },
   actions: {
     delete() {
       console.log('Delete button clicked. Inside components/restaurant-list')
       return this.sendAction('deleteRestaurant', this.get('restaurant'))
+    },
+    create() {
+      console.log('Create restaurant. Inside components/restaurant-list')
+      let data = this.get('newRestaurant')
+      data.restaurant = this.get('restaurant')
+      console.log('new restaurant is: ', this.get('newRestaurant'))
+      this.sendAction('createRestaurant', data)
+      // after sending action, clear the input fields
+      // this.set('newRestaurant.name', null)
+      // this.set('newRestaurant.city', null)
+      // this.set('newRestaurant.cuisine', null)
+
+
     }
   }
 });
